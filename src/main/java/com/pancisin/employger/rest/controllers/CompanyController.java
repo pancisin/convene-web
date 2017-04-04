6ce5ccd8ed2a8b17c09b3198d8b6148fb9fff70a
Patrik Pančišin <patrik.pancisin@ness.com>
2017-04-04 15:03:54 +0200

    Changed request mapping annotations within CompanyController.

--- src/main/java/com/pancisin/employger/rest/controllers/CompanyController.java
+++ src/main/java/com/pancisin/employger/rest/controllers/CompanyController.java
@@ -5,10 +5,12 @@ import javax.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.pancisin.employger.models.Company;
@@ -23,62 +25,63 @@ public class CompanyController {
 
 	@Autowired
 	private CompanyRepository companyRepository;
-	
+
 	@Autowired
 	private DutyRepository dutyRepository;
-	
-	@RequestMapping(value = "/", method = RequestMethod.GET)
+
+	@GetMapping("/")
 	public ResponseEntity<?> getCompanies() {
 		return ResponseEntity.ok(companyRepository.findAll());
 	}
-	
-	@RequestMapping(value = "/{company_id}", method = RequestMethod.GET)
+
+	@GetMapping("/{company_id}")
 	public ResponseEntity<?> getCompany(@PathVariable Long company_id) {
 		return ResponseEntity.ok(companyRepository.findOne(company_id));
 	}
-	
-	@RequestMapping(value = "/{company_id}", method = RequestMethod.PUT)
+
+	@PutMapping(value = "/{company_id}")
 	public ResponseEntity<?> updateCompany(@PathVariable Long company_id, @RequestBody Company company) {
 		Company stored = companyRepository.findOne(company_id);
-		
+
 		if (stored == null) {
 			return null;
 		}
-		
+
 		stored.setName(company.getName());
 		stored.setLogo(company.getLogo());
-		
+
 		companyRepository.save(stored);
 		return ResponseEntity.ok(stored);
 	}
-	
-	@RequestMapping(value = "/{company_id}/employees", method = RequestMethod.GET)
+
+	@GetMapping("/{company_id}/employees")
 	public ResponseEntity<?> getEmployees(@PathVariable Long company_id) {
 		Company company = companyRepository.findOne(company_id);
 		return ResponseEntity.ok(company.getEmployees());
 	}
-	
-	@RequestMapping(value = "/{company_id}/licenses", method = RequestMethod.GET)
+
+	@GetMapping("/{company_id}/licenses")
 	public ResponseEntity<?> getCompanyLicenses(@PathVariable Long company_id) {
 		Company company = companyRepository.findOne(company_id);
 		return ResponseEntity.ok(company.getLicenses());
 	}
-	
-	@RequestMapping(value = "/{company_id}/duties", method = RequestMethod.GET)
+
+	@GetMapping("/{company_id}/duties")
 	public ResponseEntity<?> getCompanyDuties(@PathVariable Long company_id) {
 		Company company = companyRepository.findOne(company_id);
 		return ResponseEntity.ok(dutyRepository.findByCompany(company));
 	}
-	
-	@RequestMapping(value = "/{company_id}/duties", method = RequestMethod.POST)
-	public ResponseEntity<?> createCompanyDuties(@PathVariable Long company_id, @Valid @RequestBody Duty duty, BindingResult bindingResult) {
+
+	@PostMapping("/{company_id}/duties")
+	public ResponseEntity<?> createCompanyDuties(@PathVariable Long company_id, @Valid @RequestBody Duty duty,
+			BindingResult bindingResult) {
 		Company company = companyRepository.findOne(company_id);
-		
-		if (bindingResult.hasErrors()) 
+
+		if (bindingResult.hasErrors())
 			throw new InvalidRequestException("Invalid data", bindingResult);
 
 		duty.setCompany(company);
-		
+
 		return ResponseEntity.ok(dutyRepository.save(duty));
 	}
 }
