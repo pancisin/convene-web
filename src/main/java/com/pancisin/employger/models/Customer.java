d7e15132139b6624c6eee7db05e7211bc31240cb
Patrik Pančišin <patrik.pancisin@ness.com>
2017-04-26 17:01:50 +0200

    Improved pdf reports for customer.

--- src/main/java/com/pancisin/employger/models/Customer.java
+++ src/main/java/com/pancisin/employger/models/Customer.java
@@ -1,6 +1,11 @@
 package com.pancisin.employger.models;
 
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Date;
 import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
 
 import javax.persistence.CascadeType;
 import javax.persistence.Column;
@@ -15,8 +20,11 @@ import javax.persistence.Table;
 
 import org.hibernate.validator.constraints.Email;
 import org.hibernate.validator.constraints.NotEmpty;
+import org.springframework.beans.factory.annotation.Autowired;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.pancisin.employger.repository.DutyClauseRepository;
+import com.pancisin.employger.rest.controllers.objects.DutyInstance;
 
 @Entity()
 @Table(name = "customers")
@@ -25,36 +33,55 @@ public class Customer {
 	@Id
 	@GeneratedValue(strategy = GenerationType.AUTO)
 	private Long id;
-	
+
 	@JsonIgnore
 	@ManyToOne
 	private Company company;
-	
+
 	@NotEmpty
 	@Column
 	private String name;
-	
+
 	@Column
 	private String description;
-	
+
 	@Column
 	private String person;
-	
+
 	@Email
 	@NotEmpty
 	@Column
 	private String email;
-	
+
 	@Column
 	private String address;
 
 	@JsonIgnore
 	@OneToMany(mappedBy = "customer")
 	private List<Duty> duties;
-	
+
 	@OneToOne(cascade = CascadeType.ALL)
 	private Report report = new Report();
-	
+
+//	@Autowired
+//	private DutyClauseRepository clauseRepository;
+
+	public List<DutyInstance> getDutyInstances(Date date_from, Date date_to) {
+//		List<DutyClause> clauses = clauseRepository.getRelated(date_from, date_to);
+
+		List<DutyClause> clauses = new ArrayList<DutyClause>();
+		
+		return this.duties.stream().flatMap(duty -> duty.getOcurrencesInRange(date_from, date_to).stream().map(occ -> {
+			Calendar cal = Calendar.getInstance();
+			cal.setTime(occ);
+
+			Optional<DutyClause> clause = clauses.stream().filter(dc -> dc.getPrimaryDate().equals(cal)).findFirst();
+
+			return new DutyInstance(duty, clause.isPresent() ? clause.get().getAlternativeDate() : cal,
+					clause.isPresent() ? clause.get() : null);
+		})).collect(Collectors.toList());
+	}
+
 	public Company getCompany() {
 		return company;
 	}
