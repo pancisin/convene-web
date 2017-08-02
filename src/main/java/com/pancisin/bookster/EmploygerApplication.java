708589358845d6ef940064fb7d5ac7d89a3b0cdd
Patrik Pancisin <patrik.pancisin@performgroup.com>
2017-08-02 13:33:49 +0200

    Updated pom.xml and minor changes. -> Requires bug investigation still.

--- src/main/java/com/pancisin/bookster/EmploygerApplication.java
+++ src/main/java/com/pancisin/bookster/EmploygerApplication.java
@@ -1,23 +1,19 @@
 package com.pancisin.bookster;
 
-import org.jsondoc.spring.boot.starter.EnableJSONDoc;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
 import org.springframework.cache.annotation.EnableCaching;
 //import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+//import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
-@EnableZuulProxy
+//@EnableZuulProxy
 //@EnableDiscoveryClient
 @SpringBootApplication
 @EnableScheduling
 @EnableCaching
 @EnableAutoConfiguration
-@EnableJSONDoc
-@EnableOAuth2Sso
 public class EmploygerApplication {
 
 	public static void main(String[] args) {
--- src/main/java/com/pancisin/bookster/config/SecurityConfiguration.java
+++ src/main/java/com/pancisin/bookster/config/SecurityConfiguration.java
@@ -1,15 +1,14 @@
 package com.pancisin.bookster.config;
 
-import org.springframework.boot.autoconfigure.security.SecurityProperties;
 import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.annotation.Order;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.web.csrf.CsrfFilter;
 import org.springframework.web.filter.CharacterEncodingFilter;
 
 @Configuration
+@EnableOAuth2Sso
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 	
 	@Override
