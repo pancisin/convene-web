042f8ac944a9cdc4a2b8b77390b4e7aa4e4fbf5d
Patrik Pancisin <pancisin.patrik@hotmail.com>
2017-09-07 14:03:46 +0200

    Redesigned events list to masonry list.

--- src/main/vuejs/pages/public/Event.index.vue
+++ src/main/vuejs/pages/public/Event.index.vue
@@ -6,9 +6,9 @@
         <date-picker v-model="filters.timestamp" inline="true"></date-picker>
       </div>
       <div class="col-md-9">
-        <div class="events-list">
-          <router-link v-for="event in eventsPaginator.content" :to="'/event/' + event.id" class="event-item clearfix" :key="event.id">
-            <img v-if="event.bannerUrl != null" :src="event.bannerUrl" class="hidden-xs">
+        <div class="events-masonry">
+          <router-link v-for="event in eventsPaginator.content" :to="'/event/' + event.id" class="event-item" :key="event.id">
+            <img v-if="event.bannerUrl != null" :src="event.bannerUrl">
 
             <div class="content">
               <h4 v-text="event.name"></h4>
@@ -82,46 +82,60 @@ export default {
 </script>
 
 <style lang="less">
-.events-list {
-  display: flex;
-  flex-wrap: wrap;
+.events-masonry {
+  padding: 0;
+  -moz-column-gap: 1.5em;
+  -webkit-column-gap: 1.5em;
+  column-gap: 1.5em;
 
   .event-item {
-    clear: both;
-    flex: 370px 1 1;
-    margin: .5%;
-    background: #fff;
-    border: 1px solid #ccc;
-    box-shadow: 3px 3px 10px 0px rgba(111, 110, 110, 0.3);
-    height: 125px;
-    overflow: hidden;
-    display: flex;
-    transition: 0.2s cubic-bezier(0.165, 0.84, 0.44, 1);
-    color: #000;
+    display: inline-block;
+    margin: 0 0 1.5em;
+    width: 100%;
+    box-sizing: border-box;
+    -moz-box-sizing: border-box;
+    -webkit-box-sizing: border-box;
 
-    .content {
-      display: inline-block;
-      padding: 0 10px;
-      width: 100%;
-      position: relative;
-
-      .date {
-        position: absolute;
-        bottom: 0;
-        right: 0;
-        padding: 10px;
-      }
-    }
+    background: white;
 
     img {
-      width: 200px;
-      float: left;
-      min-height: 100%;
+      width: 100%;
     }
 
-    &:hover {
-      transform: scale(1.01);
+    .content {
+      padding: 0px 10px 10px 10px;
+      color: #000;
+
+      h4 {
+        text-transform: uppercase;
+        font-size: 15px;
+        line-height: 18px;
+      }
     }
+  }
+}
+
+@media only screen and (min-width: 700px) {
+  .events-masonry {
+    -moz-column-count: 2;
+    -webkit-column-count: 2;
+    column-count: 2;
+  }
+}
+
+@media only screen and (min-width: 900px) {
+  .events-masonry {
+    -moz-column-count: 3;
+    -webkit-column-count: 3;
+    column-count: 3;
+  }
+}
+
+@media only screen and (min-width: 1100px) {
+  .events-masonry {
+    -moz-column-count: 4;
+    -webkit-column-count: 4;
+    column-count: 4;
   }
 }
 </style>
