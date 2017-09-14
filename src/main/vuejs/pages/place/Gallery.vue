d85fa3f820c5fae499d684964f13d2ad4c2b6f4d
Patrik Pančišin <pancisin.patrik@hotmail.com>
2017-09-14 23:54:11 +0200

    Updated place gallery FE.

--- src/main/vuejs/pages/place/Gallery.vue
+++ src/main/vuejs/pages/place/Gallery.vue
@@ -1,12 +1,15 @@
 <template>
-  <div class="row">
-    <div class="col-md-4" v-for="item in gallery" :key="item.id">
-      <img :src="item.path" class="img img-thumbnail">
+  <div>
+    <div class="gallery-masonry">
+      <div class="gallery-item" v-for="item in gallery" :key="item.id">
+        <img :src="item.path" class="img img-thumbnail">
+
+      </div>
     </div>
 
     <div class="col-md-4">
       <image-upload v-model="galleryItem"></image-upload>
-        <button class="btn btn-default btn-block" @click="submit">Submit</button>
+      <button class="btn btn-default btn-block" @click="submit">Submit</button>
     </div>
   </div>
 </template>
@@ -50,6 +53,47 @@ export default {
 };
 </script>
 
-<style>
+<style lang="less">
+.gallery-masonry {
+  padding: 0;
+  -moz-column-gap: 1.5em;
+  -webkit-column-gap: 1.5em;
+  column-gap: 1.5em;
 
+  .gallery-item {
+    display: inline-block;
+    margin: 0 0 1.5em;
+    width: 100%;
+    box-sizing: border-box;
+    -moz-box-sizing: border-box;
+    -webkit-box-sizing: border-box;
+
+    background: #fff;
+    box-shadow: 3px 3px 10px 0px rgba(111, 110, 110, 0.3);
+  }
+}
+
+@media only screen and (min-width: 700px) {
+  .gallery-masonry {
+    -moz-column-count: 2;
+    -webkit-column-count: 2;
+    column-count: 2;
+  }
+}
+
+@media only screen and (min-width: 900px) {
+  .gallery-masonry {
+    -moz-column-count: 3;
+    -webkit-column-count: 3;
+    column-count: 3;
+  }
+}
+
+@media only screen and (min-width: 1100px) {
+  .gallery-masonry {
+    -moz-column-count: 3;
+    -webkit-column-count: 3;
+    column-count: 3;
+  }
+}
 </style>
