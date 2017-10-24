591cee1c2422c6529259b5ecdf48aa4f1c6674a0
Patrik Pancisin <pancisin.patrik@hotmail.com>
2017-10-24 08:21:16 +0200

    feature/facebook_importer: Added google maps place search autocomplete to simpler geocoding user experience.

--- src/main/vuejs/pages/system/PageImport.vue
+++ src/main/vuejs/pages/system/PageImport.vue
@@ -1,29 +1,36 @@
 <template>
   <panel type="primary">
-    
-    <ul>
+    <label>Search places by region</label>
+    <input type="text" class="form-control" id="maps_autocomplete">
+
+    <ul v-loading="loading" class="m-t-15">
       <li v-for="(place, index) in places" :key="index">
         {{ place.name }} <a class="btn btn-success btn-xs" @click="importPage(place.id)">Import</a> 
       </li>
     </ul>
-
-
   </panel>
 </template>
 
 <script>
 import ImporterApi from 'api/importer.api';
+import GoogleMapsApiLoader from 'google-maps-api-loader';
+
 export default {
   name: 'page-import',
   data () {
     return {
       places: [],
-      subscription: null
+      subscription: null,
+      selected: null,
+      loading: false
     };
   },
+  watch: {
+    selected(newVal) {
+      console.log(newVal);
+    }
+  },
   created () {
-    this.getPlaces();
-
     this.connectWM('stomp').then(frame => {
       this.subscription = this.$stompClient.subscribe('/user/queue/page.import', response => {
         let data = JSON.parse(response.body);
@@ -31,16 +38,31 @@ export default {
         console.warn(data);
       });
     });
+
+    GoogleMapsApiLoader({
+      apiKey: 'AIzaSyBKua_eTxYYK4hJf7sRKeH666HdcH3UlAg',
+      libraries: [ 'places' ]
+    }).then(google => {
+      var input = document.getElementById('maps_autocomplete');
+      const autocomplete = new google.maps.places.Autocomplete(input);
+
+      google.maps.event.addListener(autocomplete, 'place_changed', () => {
+        const location = autocomplete.getPlace().geometry.location;
+
+        this.loading = true;
+        ImporterApi.searchPlace(location.lat(), location.lng(), places => {
+          this.places = places;
+          this.loading = false;
+        });
+      });
+    }, err => {
+      console.error(err);
+    });
   },
   beforeDestroy () {
     this.subscription.unsubscribe();
   },
   methods: {
-    getPlaces () {
-      ImporterApi.searchPlace(48.7209956, 21.2577477, places => {
-        this.places = places;
-      });
-    },
     importPage (place_id) {
       this.sendWM(`/app/page-import`, JSON.stringify({
         facebook_id: place_id
@@ -49,7 +71,3 @@ export default {
   }
 };
 </script>
-
-<style>
-
-</style>
