54588e2818356dba9b1088a68c8376c4dfb11b6d
Patrik Pančišin <pancisin.patrik@hotmail.com>
2017-11-23 13:01:35 +0100

    Added forwarding bots to index request resolver and fixed multiple major issues.

--- src/main/java/com/pancisin/bookster/controllers/IndexController.java
+++ src/main/java/com/pancisin/bookster/controllers/IndexController.java
@@ -6,14 +6,14 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
-import org.springframework.web.servlet.ModelAndView;
 
+import com.pancisin.bookster.components.SitemapView;
 import com.pancisin.bookster.utils.EnviromentProvider;
 
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 
 @Controller
-@RequestMapping("/")
 public class IndexController {
 
 	@Autowired
@@ -22,21 +22,34 @@ public class IndexController {
 	@Autowired
 	private HttpServletRequest request;
 
-	private final String crawlersPatters = "facebookexternalhit/[0-9]|Twitterbot|Pinterest|Google.*snippet";
+	@Autowired
+	private SitemapView sitemapView;
 
+	private final String crawlersPatters = "facebookexternalhit\\/[0-9](\\.[0-9])?|Facebot|Twitterbot|Pinterest|Google.*snippet";
+
+	@RequestMapping("/")
+	public String getRoot() {
+		return this.mainPage();
+	}
+	
 	@ResponseStatus(HttpStatus.OK)
-	@RequestMapping(value = "/")
-	public ModelAndView mainPage() {
+	@RequestMapping(value = "/**/{[path:[^\\.]*}")
+	public String mainPage() {
+		final String url = request.getRequestURI();
 
-		if (request.getHeader("user-agent").matches(crawlersPatters)) {
-			System.out.println(request.getRequestURL());
-			return new ModelAndView("forward://static/event/5");
+		if (request.getHeader("user-agent").matches(crawlersPatters) && !url.contains("static")) {
+			return String.format("forward://static/%s", url);
 		}
 
 		if (environmentProvider.isProduction()) {
-			return new ModelAndView("forward://dist/index.html");
+			return "forward://dist/index.html";
 		} else {
-			return new ModelAndView("forward://index.html");
+			return "forward://index.html";
 		}
+	}
+
+	@RequestMapping(path = "/sitemap.xml", produces = MediaType.APPLICATION_XML_VALUE)
+	public SitemapView create() {
+		return sitemapView;
 	}
 }
--- src/main/java/com/pancisin/bookster/controllers/StaticController.java
+++ src/main/java/com/pancisin/bookster/controllers/StaticController.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.pancisin.bookster.repository.EventRepository;
+import com.pancisin.bookster.repository.PageRepository;
 
 @Controller
 public class StaticController {
@@ -14,9 +15,18 @@ public class StaticController {
 	@Autowired
 	private EventRepository eventRepository;
 	
+	@Autowired
+	private PageRepository pageRepository;
+	
 	@RequestMapping("/static/event/{event_id}")
 	public String getEvent(@PathVariable Long event_id, Model model) {
 		model.addAttribute("entity", eventRepository.findOne(event_id));
+		return "crawlerBotStaticTemplate";
+	}
+	
+	@RequestMapping("/static/page/{page_id}")
+	public String getPage(@PathVariable Long page_id, Model model) {
+		model.addAttribute("entity", pageRepository.findOne(page_id));
 		return "crawlerBotStaticTemplate";
 	}
 }
